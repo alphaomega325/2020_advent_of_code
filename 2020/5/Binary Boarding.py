04cc09cc426f544d07db7540a739bac20fa4f5bc
alphaomega325 <alphaomega325@gmail.com>
2020-12-07 00:53:07 -0500

    Working on Star 2

--- 2020/5/Binary Boarding.py
+++ 2020/5/Binary Boarding.py
@@ -2,12 +2,13 @@
 import math
 
 def main():
-    file = open("Binary_Boarding_Examples.txt", "r")
+    file = open("Binary_Boarding_Input.txt", "r")
     rows = 127
     columns = 7
     lines = file.readlines()
     file.close()
 
+    seats = []
     highest_seat_id = 0
     
     for seat in lines:
@@ -19,13 +20,13 @@ def main():
         for element in range(len(row)):
             if(element == len(row) - 1):
                 if(row[element] == "B"):
-                    rows = upper
+                    rows = math.ceil(upper)
                 else:
-                    rows = lower
+                    rows = math.ceil(lower)
             elif(row[element] == "F"):
-                upper = math.ceil((lower + upper) / 2)
+                upper = (lower - 1 + upper) // 2
             elif(row[element] == "B"):
-                lower = math.ceil((lower + upper) / 2)
+                lower = (lower + upper + 1) // 2
 
 
         upper = 7
@@ -34,13 +35,13 @@ def main():
         for element in range(len(column)):
             if(element == len(column) - 1):
                 if(column[element] == "L"):
-                    columns = lower
+                    columns = math.ceil(lower)
                 else:
-                    columns = upper
+                    columns = math.floor(upper)
             elif(column[element] == "L"):
-                upper = math.ceil((lower + upper) / 2)
+                upper = (lower - 1 + upper) // 2
             elif(column[element] == "R"):
-                lower = math.ceil((lower + upper) / 2)
+                lower = (lower + upper + 1) // 2
                 
         
 
@@ -49,14 +50,27 @@ def main():
         if(seat_id > highest_seat_id):
             highest_seat_id = seat_id
         print(f"{seat}: row {rows}, column {columns}, seat ID {seat_id}")
-        
+        seats.append({"Seat":seat, "Row":rows, "Column":columns, "Seat ID":seat_id})
             
 
     print(f"Highest Seat ID {highest_seat_id}")
+    print(f"Highest Seat ID without the front and back rows is {show_highest_seat_id(seats)}")
     
-    
+def show_highest_seat_id(seats):
+    highest = lowest = seats[0]["Row"]
+    for item in seats:
+        if(lowest > item["Row"]):
+            lowest = item["Row"]
+        elif(highest < item["Row"]):
+            highest = item["Row"]
 
-    
+    highest_seat_id = 0
+
+    for item in seats:
+        if(highest_seat_id < item["Seat ID"] and highest != item["Row"] and lowest != item["Row"]):
+            highest_seat_id = item["Seat ID"]
+
+    return highest_seat_id
 
 
 
